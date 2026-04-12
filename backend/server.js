081bd10ebb61dda001f81a48f250069b3331ae8a
Gaurav Rao <gauravrao8382@gmail.com>
2026-04-12 15:19:46 +0530

    Render Setup

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ const startServer = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("🌐 Atlas DB connected");
 
-    app.listen(5000, () => {
-      console.log("🚀 Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log("Server running on", PORT);
     });
 
   } catch (err) {
