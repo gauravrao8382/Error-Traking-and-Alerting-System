f717677d2de62f5bb6a6c0f9ab32e170875e01bc
Gaurav Rao <gauravrao8382@gmail.com>
2026-04-12 19:05:10 +0530

    Updation for render

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,28 @@
 import nodemailer from "nodemailer";
+
 export const sendEmail = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS,
       }
     });
 
-    
     const info = await transporter.sendMail({
-      from: `"Error-Tracker" <${process.env.EMAIL_USER}>`, // ✅ FIXED
+      from: `"Error-Tracker" <${process.env.BREVO_USER}>`,
       to: email,
-      subject: "Error-Tracker - Email Verification OTP",
+      subject: "Error-Tracker - OTP Verification",
       text: `Your OTP is: ${otp}`
     });
 
     console.log("✅ Email sent:", info.response);
 
   } catch (error) {
-    console.log("❌ EMAIL ERROR:", error); // 🔥 THIS WILL SHOW REAL ISSUE
+    console.log("❌ EMAIL ERROR:", error.message);
     throw error;
   }
 };
